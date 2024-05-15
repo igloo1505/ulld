44e51e4eb1cfcf87caf2a57f1d25138516eb6232
Uh-little-less-dum <uhlittlelessdumb@mail.com>
2024-05-14 22:33:40 -0500

    A shot in the dark

--- apps/website/next.config.mjs
+++ apps/website/next.config.mjs
@@ -13,16 +13,16 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 // import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 
 // NOTE: For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
-if (
-  process.env.LD_LIBRARY_PATH == null ||
-  !process.env.LD_LIBRARY_PATH?.includes(
-    `${process.env.PWD}/node_modules/canvas/build/Release:`,
-  )
-) {
-  process.env.LD_LIBRARY_PATH = `${
-    process.env.PWD
-  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
-}
+// if (
+//   process.env.LD_LIBRARY_PATH == null ||
+//   !process.env.LD_LIBRARY_PATH?.includes(
+//     `${process.env.PWD}/node_modules/canvas/build/Release:`,
+//   )
+// ) {
+//   process.env.LD_LIBRARY_PATH = `${
+//     process.env.PWD
+//   }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ''}`;
+// }
 
 const mermaidTheme = {
     light: {
