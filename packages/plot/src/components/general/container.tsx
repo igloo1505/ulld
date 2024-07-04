9eadebfd02514cabb8bb72c23104c17801ccf25b
Uh-little-less-dum <uhlittlelessdumb@mail.com>
2024-07-03 19:02:12 -0500

    Fixe some ssr bugs.

--- packages/plot/src/components/general/container.tsx
+++ packages/plot/src/components/general/container.tsx
@@ -1,27 +1,26 @@
-import React, { ReactNode } from "react";
+import React, { ReactElement, ReactNode } from "react";
 import { BasePlotProps } from "../../types";
 import { InvalidVariablesWarning } from "./invalidVariables";
 import {
-  ComposedChart,
-  Line,
-  Area,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  Scatter,
-  ResponsiveContainer,
-} from 'recharts';
+    ComposedChart,
+    Line,
+    Area,
+    Bar,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend,
+    Scatter,
+    ResponsiveContainer,
+} from "recharts";
 
 export const PlotContainer = ({
     title,
     children,
     validVariables,
     desc,
-    noGrid,
-}: BasePlotProps & { children: ReactNode; validVariables: boolean }) => {
+}: BasePlotProps & { children: ReactElement; validVariables: boolean }) => {
     if (!validVariables) {
         return <InvalidVariablesWarning />;
     }
@@ -29,7 +28,7 @@ export const PlotContainer = ({
         <div className={"w-full h-fit max-h-[min(600px,80vh)] inline-block"}>
             {title && <h3 className={"text-lg font-semibold"}>{title}</h3>}
             <ResponsiveContainer width="100%" height="100%">
-                    {children}
+                {children}
             </ResponsiveContainer>
             {desc && <p className={"text-sm text-muted-foreground"}>{desc}</p>}
         </div>
