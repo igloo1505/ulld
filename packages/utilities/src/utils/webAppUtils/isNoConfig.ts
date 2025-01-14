44cad2f7f45581fac8e759b877d31999b3de5529
Uh-little-less-dum <uhlittlelessdumb@gmail.com>
2025-01-14 15:17:02 -0600

    Fix(Build): Broke build
    Somehow broke prisma generate in build script.

--- packages/utilities/src/utils/webAppUtils/isNoConfig.ts
+++ packages/utilities/src/utils/webAppUtils/isNoConfig.ts
@@ -1,5 +1,5 @@
 import type { IsNoConfigProps } from "@ulld/types"
 
 export const isNoConfig = (props?: IsNoConfigProps): boolean => {
-    return Boolean(props?.noConfig || process.env.ULLD_NO_CONFIG)
+    return Boolean(props?.noConfig || process.env.ULLD_NO_CONFIG === "true")
 }
